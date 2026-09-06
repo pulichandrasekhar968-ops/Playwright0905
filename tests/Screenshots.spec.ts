6dc5527811dd81545dbbcd0973a995aaf7d848ab
pulichandrasekhar968-ops <pulichandrasekhar968@gmail.com>
2026-09-06 10:40:33 +0530

    SS

--- tests/Screenshots.spec.ts
+++ tests/Screenshots.spec.ts
@@ -4,8 +4,12 @@ test ('screenshot', async({page})=>{
 
     await page.goto ("https://testautomationpractice.blogspot.com/")
 
+    const timestamp = Date.now()
+
     //await page.screenshot({path:'Screenshots/loginpage.png'});
 
-    await page.screenshot({path:'Screenshots/Fullloginpage.png',fullPage:true});
+    //await page.screenshot({path:'Screenshots/Fullloginpage.png',fullPage:true});
+
+    await page.screenshot({path:'Screenshots/'+'lobloginpage.png'+ timestamp +'.png'});
 
 })
